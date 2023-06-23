6e1343442b4903c2cbc9d2140660e12c3d7fefb1
Sujal Soni <112640952+Sujal-2820@users.noreply.github.com>
2023-06-23 13:29:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfrywqUjSRomyQKpV6ISMoEug9w7PZ8f0",
-  authDomain: "auth-app-f3957.firebaseapp.com",
-  projectId: "auth-app-f3957",
-  storageBucket: "auth-app-f3957.appspot.com",
-  messagingSenderId: "519326465220",
-  appId: "1:519326465220:web:2a3ef217691243763ddd91",
-  measurementId: "G-ZKYHPGCJVP"
+  //Paste your credentials here
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
